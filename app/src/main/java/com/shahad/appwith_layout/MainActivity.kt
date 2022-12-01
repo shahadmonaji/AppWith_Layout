81b15955cb55248161c66a640452d27119df0ea0
شهد بنت الميزاني المطيري <438002080@pnu.edu.sa>
2022-12-01 13:33:19 +0300

    Initial commit

---
+++ app/src/main/java/com/shahad/appwith_layout/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.shahad.appwith_layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
